57c47bdb2b67c067b0ed7dffbcb8921c750e2c04
harquail <nook@harquail.com>
2015-07-26 17:46:28 -0400

    gitignore api keys

---
+++ ParseStarterProject/APIKeysAndSecrets.swift
@@ -0,0 +1,13 @@
+//
+//  APIKeysAndSecrets.swift
+//  ParseStarterProject
+//
+//  Created by nook on 7/26/15.
+//  Copyright (c) 2015 Parse. All rights reserved.
+//
+
+import Foundation
+
+let kForecastAPIKey = "a4201b552b5b4a8abcd15ad8e749a5d0"
+let kParseApplicationID = "XoQrsMloGEqItCRd0oKsHQwmOZ0gD9kodljCJNYn"
+let kParseClientKey = "r66dKh1OHhseQ0YpDM7VrregBjyyBe0cLcixNBih"
